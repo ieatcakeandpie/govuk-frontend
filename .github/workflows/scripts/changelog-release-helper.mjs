1521c0e0de23f7662dade27b5030103a03069087
Owen Jones <owen.jones@digital.cabinet-office.gov.uk>
2025-02-03 17:17:34 +0000

    Don't write to changelog if version is an internal pre-release

--- .github/workflows/scripts/changelog-release-helper.mjs
+++ .github/workflows/scripts/changelog-release-helper.mjs
@@ -104,6 +104,18 @@ export function validateVersion(newVersion) {
  * @param {string} newVersion
  */
 export function updateChangelog(newVersion) {
+  // Skip the entire function if the release version is internal eg: 5.1.0-internal.0
+  if (versionIsAPrerelease(newVersion)) {
+    const identifier = getPrereleaseIdentifierAndBase(newVersion)[0]
+
+    if (identifier === 'internal') {
+      console.log(
+        'This is an internal release, intended for testing only. The changelog will therefore not be updated.'
+      )
+      return
+    }
+  }
+
   const changelogLines = getChangelogLines()
   const [startIndex, previousReleaseLineIndex] =
     getChangelogLineIndexes(changelogLines)
